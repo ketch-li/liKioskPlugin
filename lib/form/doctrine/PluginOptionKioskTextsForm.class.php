e51beec7d69b40d644428ac634447d0a5439ce45
Romain Sanchez <romain.sanchez@libre-informatique.fr>
2017-11-15 15:47:28 +0100

    Fix form class name

--- lib/form/doctrine/PluginOptionKioskTextsForm.class.php
+++ lib/form/doctrine/PluginOptionKioskTextsForm.class.php
@@ -32,7 +32,7 @@
  * @author     Your name here
  * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class OptionKioskTextsForm extends BaseOptionKioskTextsForm
+class PluginOptionKioskTextsForm extends BaseOptionKioskTextsForm
 {
   /**
    * @see OptionForm
